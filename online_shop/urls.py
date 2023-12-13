20f26521c7506a6d29fbfb2d3a19389b77f61971
hesel <hgharehbaai@gmail.com>
2023-12-14 01:09:32 +0330

    feat: add products and its details to home page

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .settings import MEDIA_ROOT, MEDIA_URL
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
-]
+] + static( MEDIA_URL, document_root= MEDIA_ROOT)
