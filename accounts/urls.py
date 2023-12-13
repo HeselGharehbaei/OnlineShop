cd36232a288ac9e964a47ad068b9b1e94bd818f1
hesel <hgharehbaai@gmail.com>
2023-12-13 17:39:07 +0330

    feat: manage static files

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ app_name= 'accounts'
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name= 'user_register'),
     path('verify/', views.UserRegisterVerifyCodeView.as_view(), name= 'verify_code'),
-    path('login', views.UserLoginView.as_view(), name='login'),
-    path('logout', views.UserLogoutView.as_view(), name='logout'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
 ]
